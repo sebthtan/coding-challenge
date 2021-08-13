c2ec686ad60179e44595dd1fd2d068d7f1ed659b
Sebastian Tan <sebthtan@yahoo.com>
2021-08-13 14:55:34 -0500

    #4 completed

--- exercises/loneSome.js
+++ exercises/loneSome.js
@@ -1,6 +1,8 @@
 module.exports.run = function(a, b, c) {
 /*
-Given 3 int values, a b c, return their sum. However, if one of the values is the same as another of the values, it does not count towards the sum.
+Given 3 int values, a b c, return their sum. 
+However, if one of the values is the same as another of the values, 
+it does not count towards the sum.
 
 Examples:
 	function(1, 2, 3) => 6
@@ -10,5 +12,17 @@ Examples:
 Write your code below the comment.
 
 */
+
+	if (a === b && a === c) {
+		return 0
+	} else if (a === b) {
+		return c
+	} else if (b === c) {
+		return a
+	} else if (a === c) {
+		return b
+	} else {
+		return a + b + c
+	}
 
 };
