f2737055a13d5c45440be2b142e2f92d04c86831
Sebastian Tan <sebthtan@yahoo.com>
2021-08-12 22:22:45 -0500

    #3 completed

--- exercises/gHappy.js
+++ exercises/gHappy.js
@@ -9,5 +9,29 @@ Return true if all the g's in the given string are happy, otherwise return false
 
         Write your code below the comment.
 */
+	if (str.length < 2) return false
 
+	let count = 0
+	let idx = str.indexOf('g')
+	const visited = []
+	let happy = true
+
+	while (idx !== -1) {
+		count ++
+		visited.push(idx)
+		idx = str.indexOf('g', idx + 1)
+	}
+	console.log(visited)
+
+	visited.forEach(idx => {
+		const prev = str[idx - 1].toLowerCase()
+		const current = str[idx].toLowerCase()
+		const next = str[idx + 1] ? str[idx + 1].toLowerCase() : '_'
+
+		if (current === 'g' && prev !== 'g' && next !== 'g') {
+			happy = false
+		}
+	})
+
+	return happy
 };
